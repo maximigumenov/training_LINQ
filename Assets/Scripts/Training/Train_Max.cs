83bd0d620602432b8ef1f3db104db0c9a410f87e
Max <unity3damcon@gmail.com>
2020-09-03 14:13:11 +0300

    111

---
+++ Assets/Scripts/Training/Train_Max.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Train_Max : BaseTrain
+{
+    public override void FullWriting()
+    {
+        
+    }
+
+    public override void ExtensionWriting()
+    {
+        oneObject = list.Max()
+    }
+
+
+    public override void ShowExtension()
+    {
+        base.ShowExtension();
+        Debug.Log("ShowExtension " + resultBool.ToString());
+    }
+
+    public override void ShowFull()
+    {
+        base.ShowFull();
+        Debug.Log("ShowFull " + resultBool.ToString());
+    }
+}
